2b51380334f8691abce542d10a9ce278148f8f3b
estradax <azharumuhammad@gmail.com>
2023-04-24 20:03:22 +0700

    feat: add validation and error handling

--- cmd/account/handler/auth/register.go
+++ cmd/account/handler/auth/register.go
@@ -4,42 +4,66 @@ import (
 	"github.com/estradax/exater/internal/model"
 	"github.com/estradax/exater/internal/model/user"
 	"github.com/estradax/exater/internal/session"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Register(ctx *fiber.Ctx) error {
-	url, _ := ctx.GetRouteURL("registerUser", fiber.Map{})
+	url, err := ctx.GetRouteURL("registerUser", fiber.Map{})
+	if err != nil {
+		return err
+	}
+
 	return ctx.Render("register", fiber.Map{
 		"RegisterUserURL": url,
 	})
 }
 
 type registerForm struct {
-	Name     string `form:"name"`
-	Email    string `form:"email"`
-	Password string `form:"password"`
+	Name     string `form:"name" validate:"required"`
+	Email    string `form:"email" validate:"required,email"`
+	Password string `form:"password" validate:"required,min=8"`
 }
 
 func RegisterUser(ctx *fiber.Ctx) error {
-	// TODO: Validate data.
 	p := new(registerForm)
-	_ = ctx.BodyParser(p)
+	if err := ctx.BodyParser(p); err != nil {
+		return err
+	}
 
-	_, found, _ := user.FindByEmail(p.Email)
+	validate := validator.New()
+	if err := validate.Struct(p); err != nil {
+		return err
+	}
+
+	_, found, err := user.FindByEmail(p.Email)
+	if err != nil {
+		return err
+	}
 
 	if found {
 		// TODO: Notify record already exist.
 		return ctx.RedirectToRoute("register", fiber.Map{})
 	}
 
-	u, _ := user.Create(model.User{
+	u, err := user.Create(model.User{
 		Name:     p.Name,
 		Email:    p.Email,
 		Password: p.Password,
 	})
+	if err != nil {
+		return err
+	}
 
-	sess, _ := session.SetUser(ctx, u)
-	_ = sess.Save()
+	sess, err := session.SetUser(ctx, u)
+	if err != nil {
+		return err
+	}
+
+	err = sess.Save()
+	if err != nil {
+		return err
+	}
 
 	return ctx.RedirectToRoute("account", fiber.Map{})
 }
--- go.mod
+++ go.mod
@@ -13,11 +13,15 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.12.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/leodido/go-urn v1.2.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -30,8 +34,10 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.45.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
